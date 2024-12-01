020931fc4cb607a99a8e47659f2996d4ccaab173
DmitriyGulevich <kaskodim@gmail.com>
2024-12-01 11:30:45 +0300

    first commit add README

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Здесь скоро будет приложение со списком дел :)
+    </div>
+  );
+}
+
+export default App;
